29352c113a5f182034badbb250903021879213a7
Jishnu73 <100125048+Jishnu73@users.noreply.github.com>
2022-03-03 20:21:26 +0530

    Add files via upload

---
+++ ShapeApp.java
@@ -0,0 +1,22 @@
+package trainingapi;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c=new Circle();
+		c.input();
+		c.Calculate();
+		Triangle t=new Triangle();
+		t.input();
+        t.Calculate();
+        Square s=new Square();
+        s.input();
+        s.Calculate();
+        Rectangle r=new Rectangle();
+        r.input();
+        r.Calculate();
+       		
+	}
+
+}
